c757170b7bc29c8257d0e234099e994bb9ff6171
Vasilis Pantelis <bpantelis10@gmail.com>
2019-07-27 22:37:28 +0300

    tp back to overworld

--- TwitterCityMod/src/main/java/org/twittercity/twittercitymod/commands/TwitterCityCmdTeleport.java
+++ TwitterCityMod/src/main/java/org/twittercity/twittercitymod/commands/TwitterCityCmdTeleport.java
@@ -1,11 +1,14 @@
 package org.twittercity.twittercitymod.commands;
 
+import org.twittercity.twittercitymod.teleport.TeleportationTools;
 import org.twittercity.twittercitymod.worldgen.TwitterCityWorldGenReference;
-import org.twittercity.twittercitymod.worldgen.teleport.TeleportationTools;
 
+import net.minecraft.client.resources.I18n;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
 
 public class TwitterCityCmdTeleport extends AbstractTwitterCityCommand {
 	@Override
@@ -15,23 +18,15 @@ public class TwitterCityCmdTeleport extends AbstractTwitterCityCommand {
 
 	@Override
 	public String getUsage(ICommandSender sender) {
-		return "Teleports you to Twitter City dimension.";
+		return "Teleports you to Twitter City dimension if you are not there and two overworld if you are there.";
 	}
 	
 	@Override
 	public void execute(MinecraftServer server, ICommandSender sender, String[] args) {
-		/*if (args.length < 4) {
-			ITextComponent component = new TextComponentString(TextFormatting.RED + "Several parameters are missing!");
-			super.sendMessage(sender, component);
-			return;
-		} else if (args.length > 4) {
-			ITextComponent component = new TextComponentString(TextFormatting.RED + "Too many parameters!");
-			super.sendMessage(sender, component);
-			return;
-		}*/
 
-		int dim = TwitterCityWorldGenReference.DIM_ID;//fetchInt(sender, args, 1, 0);
-		int x = fetchInt(sender, args, 2, 0);
+		int twitterCityDim = TwitterCityWorldGenReference.DIM_ID;
+		// Maybe make the default value to First city spawn point
+		int x = fetchInt(sender, args, 2, 0); 
 		int y = fetchInt(sender, args, 3, 100);
 		int z = fetchInt(sender, args, 4, 0);
 
@@ -39,11 +34,20 @@ public class TwitterCityCmdTeleport extends AbstractTwitterCityCommand {
 			EntityPlayer player = (EntityPlayer) sender;
 
 			int currentId = player.getEntityWorld().provider.getDimension();
-			if (currentId != dim) {
-				TeleportationTools.teleportToDimension(player, dim, x, y, z); // Maybe teleport to first town coordinates
+			if (currentId != twitterCityDim) {
+				// Maybe teleport to first town coordinates
+				TeleportationTools.teleportToDimension(player, twitterCityDim, x, y, z); 
+				player.sendMessage(new TextComponentString(I18n.format("twittercity.teleport.welcome")));
 			} else {
-				TeleportationTools.teleportToDimension(player, -1, x, y, z);
-				//player.setPositionAndUpdate(x, y, z);
+				BlockPos spawnPoint = player.getBedLocation(0); //Teleport to players spawn point
+				if(spawnPoint != null) {
+					TeleportationTools.teleportToDimension(player, 0, spawnPoint.getX(), spawnPoint.getY(), spawnPoint.getZ());
+				} else {
+					TeleportationTools.teleportToDimension(player, 0, 0, 63, 0);
+					BlockPos worldSpawn = player.getEntityWorld().getSpawnPoint();
+					player.setPositionAndUpdate(worldSpawn.getX(), 70, worldSpawn.getZ());
+				}
+				
 			}
 		}
 
