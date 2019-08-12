9b64067862281bc671020169e6c07c6b0cb48223
Vasilis Pantelis <bpantelis10@gmail.com>
2019-08-12 23:31:45 +0300

    gui adj

--- TwitterCityMod/src/main/java/org/twittercity/twittercitymod/data/Tweet/Tweet.java
+++ TwitterCityMod/src/main/java/org/twittercity/twittercitymod/data/Tweet/Tweet.java
@@ -1,20 +1,64 @@
 package org.twittercity.twittercitymod.data.Tweet;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
 public class Tweet {
 
 	private String text;
 	private String author;
-	
+	private String profilePicUrl;
+	private BufferedImage image = null;
+
 	public Tweet() {
 		text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed tempus urna et pharetra pharetra. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Vitae sapien pellentesque habitant morbi. Ultrices sagittis orci a scelerisque purus semper eget duis at. Ornare quam viverra orci sagittis eu volutpat odio facilisis mauris. Dignissim sodales ut eu sem integer vitae. Mattis ullamcorper velit sed ullamcorper morbi tincidunt. Egestas maecenas pharetra convallis posuere. Nec feugiat in fermentum posuere. Neque sodales ut etiam sit amet nisl purus in mollis. Pellentesque sit amet porttitor eget dolor morbi non. Justo donec enim diam vulputate ut. Aenean et tortor at risus viverra adipiscing at in tellus. Pellentesque sit amet porttitor eget dolor morbi non. Nisi scelerisque eu ultrices vitae. Libero justo laoreet sit amet cursus sit amet. Vitae justo eget magna fermentum iaculis eu non. Posuere morbi leo urna molestie at elementum eu facilisis sed. Condimentum vitae sapien pellentesque habitant. In fermentum posuere urna nec tincidunt praesent semper. Bibendum at varius vel pharetra vel turpis nunc eget. Arcu dui vivamus arcu felis. Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit. Nunc congue nisi vitae suscipit tellus. In mollis nunc sed id semper.";
 		author = "vasilis";
+		profilePicUrl = "https://pbs.twimg.com/profile_images/880136122604507136/xHrnqf1T_normal.jpg";
 	}
-	
-	public String getText() { 
+
+	public String getText() {
 		return text;
 	}
 
 	public String getAuthor() {
 		return author;
+	}
+
+	public String getgetProfilePicUrl() {
+		return author;
+	}
+
+	public BufferedImage getProfilePicture() {
+		if(this.image != null) {
+			return this.image;
+		}
+		try {
+			URL imageURL = new URL(profilePicUrl);
+			BufferedImage jpgImage = ImageIO.read(imageURL);
+			ByteArrayOutputStream byteArrayOut = new ByteArrayOutputStream();
+			byteArrayOut.flush();
+			ImageIO.write(jpgImage, "png", byteArrayOut);
+			byteArrayOut.close();
+			byte[] resultingBytes = byteArrayOut.toByteArray();
+			
+			InputStream in = new ByteArrayInputStream(resultingBytes);
+			BufferedImage oldImage = ImageIO.read(in);
+			
+			image = new BufferedImage(256, 256, BufferedImage.TYPE_INT_ARGB);
+		    Graphics g = image.getGraphics();
+		    g.drawImage(oldImage, 0, 0, null);
+		    g.dispose();
+		    
+			return this.image;
+		} catch (Exception e) {
+			this.image = null;
+		}		
+		return this.image;
 	}
 }
--- TwitterCityMod/src/main/java/org/twittercity/twittercitymod/gui/TCGuiTweet.java
+++ TwitterCityMod/src/main/java/org/twittercity/twittercitymod/gui/TCGuiTweet.java
@@ -1,5 +1,6 @@
 package org.twittercity.twittercitymod.gui;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,13 +10,14 @@ import javax.annotation.Nullable;
 import org.twittercity.twittercitymod.Reference;
 import org.twittercity.twittercitymod.data.Tweet.Tweet;
 
-import com.google.gson.JsonParseException;
+import com.google.common.collect.Lists;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiUtilRenderComponents;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.texture.DynamicTexture;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
@@ -28,32 +30,26 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class TCGuiTweet extends GuiScreen {
 	private static final ResourceLocation TEXTURE = new ResourceLocation(Reference.MOD_ID, "textures/gui/tweet.png");
 	public static final int WIDTH = 256, HEIGHT = 140;
-
+	
 	private Tweet tweet;
+	BufferedImage profileImage;
 	private List<ITextComponent> cachedComponents;
+	private List<ArrayList<ITextComponent>> textComponentPages;
 
-	private final int tweetWidth = 252;
-	//private final int tweetTextAreaHeight = 97;
+	// Should be final but for debugging ignore
+	private int TWEET_TEXT_MAX_ROW_LENGTH = 191;
+	// private final int tweetTextAreaHeight = 97;
 
 	private TCGuiTweet.NextPageButton buttonNextPage;
 	private TCGuiTweet.NextPageButton buttonPreviousPage;
 
-	private int totalPages = 1;
+	private int totalPages = 0;
 	private int currPage;
-
-	private ArrayList<String> textPages;
 
 	public TCGuiTweet(Tweet tweet) {
 		this.tweet = tweet;
-
-		if (this.textPages == null)
-        {
-            this.textPages = new ArrayList<String>();
-            this.textPages.add("");
-            this.totalPages = 1;
-        }
+		profileImage = tweet.getProfilePicture();
 	}
-
 
 	@Override
 	public boolean doesGuiPauseGame() {
@@ -63,68 +59,101 @@ public class TCGuiTweet extends GuiScreen {
 	@Override
 	public void initGui() {
 		super.initGui();
-		int i = (this.width - tweetWidth) / 2;
-		this.buttonNextPage = (TCGuiTweet.NextPageButton) this.addButton(new TCGuiTweet.NextPageButton(1, i + 200, 115, true));
-		this.buttonPreviousPage = (TCGuiTweet.NextPageButton) this.addButton(new TCGuiTweet.NextPageButton(2, i + 35, 115, false));
+		int x = (this.width - WIDTH) / 2;
+		int y = (this.height - HEIGHT) / 2;
+		this.buttonNextPage = (TCGuiTweet.NextPageButton) this
+				.addButton(new TCGuiTweet.NextPageButton(1, x + 200, y + 115, true));
+		this.buttonPreviousPage = (TCGuiTweet.NextPageButton) this
+				.addButton(new TCGuiTweet.NextPageButton(2, x + 35, y + 115, false));
 		this.updateButtons();
 	}
 
 	private void updateButtons() {
-		this.buttonNextPage.visible = true;//(this.currPage < this.totalPages - 1);
-		this.buttonPreviousPage.visible = true;//this.currPage > 0;
+		this.buttonNextPage.visible = (this.currPage < this.totalPages - 1);
+		this.buttonPreviousPage.visible = this.currPage > 0;
+	}
+
+	private void createTextPages(String text) {
+
+		ITextComponent itextcomponent = new TextComponentString(text);
+		this.cachedComponents = itextcomponent != null
+				? GuiUtilRenderComponents.splitText(itextcomponent, TWEET_TEXT_MAX_ROW_LENGTH, this.fontRenderer, true, true)
+				: null;
+
+		int tweetRowsPerPage = Math.min(65 / this.fontRenderer.FONT_HEIGHT, this.cachedComponents.size());
+		
+		int counter = 0, pageCounter = 0;
+		this.textComponentPages = new ArrayList<ArrayList<ITextComponent>>();
+		List<ITextComponent> tempList = Lists.<ITextComponent>newArrayList();
+		for (ITextComponent component : this.cachedComponents) {
+			if (counter <= tweetRowsPerPage) {
+				tempList.add(component);
+				counter++;
+			} else {
+				this.textComponentPages.add((ArrayList<ITextComponent>) tempList);
+				tempList = Lists.<ITextComponent>newArrayList();
+				counter = 0;
+				pageCounter++;
+				this.addNewPage();
+			}
+		}
+		if(!tempList.isEmpty()) {
+			this.textComponentPages.add(pageCounter, (ArrayList<ITextComponent>) tempList);
+			this.addNewPage();
+		}
+		this.updateButtons();
 	}
 
 	@Override
 	public void drawScreen(int mx, int my, float partialTicks) {
 		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
 		this.mc.getTextureManager().bindTexture(TEXTURE);
-		int i = (this.width - 252) / 2;
-		this.drawTexturedModalRect(i, 2, 0, 0, 252, 140);
+		int x = (this.width - WIDTH) / 2;
+		int y = (this.height - 140) / 2;
+		this.drawTexturedModalRect(x, y, 0, 0, WIDTH, 140);
+
+		if (this.textComponentPages == null) {
+			this.createTextPages(this.tweet.getText());
+		}
 
 		String s1 = I18n.format("book.editTitle");
 		int k = this.fontRenderer.getStringWidth(s1);
-		this.fontRenderer.drawString(s1, i + 36 + (116 - k) / 2, 34, 0);
-		String s2 = I18n.format("book.byAuthor", tweet.getAuthor());
-		int i1 = this.fontRenderer.getStringWidth(s2);
-		this.fontRenderer.drawString(TextFormatting.RED + s2, i + 36 + (116 - i1) / 2, 60, 0);
-		String s5;
-		String s4 = I18n.format("book.pageIndicator", this.currPage + 1, this.totalPages);
-		if (textPages != null && this.currPage >= 0 && this.currPage < textPages.size()) {
-			s5 = textPages.get(this.currPage);
-		}
-		s5 = tweet.getText();
-		//this.pageInsertIntoCurrent(s5);
+		this.fontRenderer.drawString(s1, x + 36 + (180 - k) / 2, y + 15, 0);
 		
-		try {
-			ITextComponent itextcomponent = ITextComponent.Serializer.jsonToComponent(s5);
-			this.cachedComponents = itextcomponent != null
-					? GuiUtilRenderComponents.splitText(itextcomponent, 191, this.fontRenderer, true, true)
-					: null;
-		} catch (JsonParseException var13) {
-			this.cachedComponents = null;
-		}
+		String bookAuthor = I18n.format("book.byAuthor", tweet.getAuthor());
+		int i1 = this.fontRenderer.getStringWidth(bookAuthor);
+		this.fontRenderer.drawString(TextFormatting.RED + bookAuthor, x + 36 + (180 -
+		i1) / 2, y + 115, 0);
+		
+		String pagesCountText = I18n.format("book.pageIndicator", this.currPage + 1, this.totalPages);
+		int j1 = this.fontRenderer.getStringWidth(pagesCountText);
+		this.fontRenderer.drawString(TextFormatting.RED + pagesCountText, x - j1 + 285 - 44, y + 15, 0);
 
-		int j1 = this.fontRenderer.getStringWidth(s4);
-		this.fontRenderer.drawString(s4, i - j1 + 252 - 44, 18, 0);
-
-		if (this.cachedComponents == null) {
-			this.fontRenderer.drawSplitString(s5, i + 36, 34, 116, 0);
-		} else {
-			int k1 = Math.min(128 / this.fontRenderer.FONT_HEIGHT, this.cachedComponents.size());
-
-			for (int l1 = 0; l1 < k1; ++l1) {
-				ITextComponent itextcomponent2 = this.cachedComponents.get(l1);
-				this.fontRenderer.drawString(itextcomponent2.getUnformattedText(), i + 36,
-						34 + l1 * this.fontRenderer.FONT_HEIGHT, 0);
-			}
-
-			ITextComponent itextcomponent1 = this.getClickedComponentAt(mx, my);
-
-			if (itextcomponent1 != null) {
-				this.handleComponentHover(itextcomponent1, mx, my);
+		if(this.currPage <= this.totalPages - 1) {
+			for (int l1 = 0; l1 < this.textComponentPages.get(this.currPage).size(); ++l1) {
+				ITextComponent itextcomponent2 = this.textComponentPages.get(this.currPage).get(l1);
+				this.fontRenderer.drawString(TextFormatting.WHITE + itextcomponent2.getUnformattedText(), x + 36,
+						y + 40 + l1 * this.fontRenderer.FONT_HEIGHT, 0);
 			}
 		}
 
+		ITextComponent itextcomponent1 = this.getClickedComponentAt(mx, my);
+		if (itextcomponent1 != null) {
+			this.handleComponentHover(itextcomponent1, mx, my);
+		}
+		
+		ResourceLocation placeholderRS = new ResourceLocation(Reference.MOD_ID, "textures/gui/placeholder.png");
+		this.mc.getTextureManager().bindTexture(placeholderRS);
+		this.drawTexturedModalRect((x + WIDTH - 70) / 2, y + 6, 0, 0, 24, 24);
+			
+		if(profileImage != null) {
+			DynamicTexture dynTextures = new DynamicTexture(profileImage);
+			//dynTextures.updateDynamicTexture();
+			ResourceLocation imRS = this.mc.getTextureManager().getDynamicTextureLocation(Reference.MOD_ID, dynTextures);
+			this.mc.getTextureManager().deleteTexture(placeholderRS);
+			this.mc.getTextureManager().bindTexture(imRS);
+			this.drawTexturedModalRect((x + WIDTH - 70) / 2, y + 6, 0, 0, 48, 48);
+		}
 		super.drawScreen(mx, my, partialTicks);
 	}
 
@@ -133,11 +162,6 @@ public class TCGuiTweet extends GuiScreen {
 			if (button.id == 1) {
 				if (this.currPage < this.totalPages - 1) {
 					++this.currPage;
-				} else {
-					this.addNewPage();
-					if (this.currPage < this.totalPages - 1) {
-						++this.currPage;
-					}
 				}
 			} else if (button.id == 2) {
 				if (this.currPage > 0) {
@@ -149,95 +173,48 @@ public class TCGuiTweet extends GuiScreen {
 	}
 
 	private void addNewPage() {
-		if (this.textPages != null) {
-			this.textPages.add("");
-			++this.totalPages;
-		}
+		++this.totalPages;
 	}
 
-	/**
-	 * Processes any text getting inserted into the current page, enforcing the page
-	 * size limit
-	 */
-	private void pageInsertIntoCurrent(String p_146459_1_) {
-		String s = this.pageGetCurrent();
-		String s1 = s + p_146459_1_;
-		int i = this.fontRenderer.getWordWrappedHeight(s1 + "" + TextFormatting.BLACK + "_", 118);
-
-		if (i <= 128 && s1.length() < 256) {
-			this.pageSetCurrent(s1);
-		}
-	}
-
-	/**
-	 * Sets the text of the current page as determined by currPage
-	 */
-	private void pageSetCurrent(String p_146457_1_) {
-		if (this.textPages != null && this.currPage >= 0) {
-			textPages.add(this.currPage, p_146457_1_);
-		}
-	}
-
-	/**
-	 * Returns the entire text of the current page as determined by currPage
-	 */
-	private String pageGetCurrent() {
-		return this.currPage >= 0 ? textPages.get(this.currPage) : "";
-	}
-
-	
 	@Nullable
-    public ITextComponent getClickedComponentAt(int p_175385_1_, int p_175385_2_)
-    {
-        if (this.cachedComponents == null)
-        {
-            return null;
-        }
-        else
-        {
-            int i = p_175385_1_ - (this.width - 252) / 2 - 36;
-            int j = p_175385_2_ - 2 - 16 - 16;
+	public ITextComponent getClickedComponentAt(int p_175385_1_, int p_175385_2_) {
+		if (this.cachedComponents == null) {
+			return null;
+		} else {
+			int i = p_175385_1_ - (this.width - 252) / 2 - 36;
+			int j = p_175385_2_ - 2 - 16 - 16;
 
-            if (i >= 0 && j >= 0)
-            {
-                int k = Math.min(128 / this.fontRenderer.FONT_HEIGHT, this.cachedComponents.size());
+			if (i >= 0 && j >= 0) {
+				int k = Math.min(128 / this.fontRenderer.FONT_HEIGHT, this.cachedComponents.size());
 
-                if (i <= 116 && j < this.mc.fontRenderer.FONT_HEIGHT * k + k)
-                {
-                    int l = j / this.mc.fontRenderer.FONT_HEIGHT;
+				if (i <= 116 && j < this.mc.fontRenderer.FONT_HEIGHT * k + k) {
+					int l = j / this.mc.fontRenderer.FONT_HEIGHT;
 
-                    if (l >= 0 && l < this.cachedComponents.size())
-                    {
-                        ITextComponent itextcomponent = this.cachedComponents.get(l);
-                        int i1 = 0;
+					if (l >= 0 && l < this.cachedComponents.size()) {
+						ITextComponent itextcomponent = this.cachedComponents.get(l);
+						int i1 = 0;
 
-                        for (ITextComponent itextcomponent1 : itextcomponent)
-                        {
-                            if (itextcomponent1 instanceof TextComponentString)
-                            {
-                                i1 += this.mc.fontRenderer.getStringWidth(((TextComponentString)itextcomponent1).getText());
+						for (ITextComponent itextcomponent1 : itextcomponent) {
+							if (itextcomponent1 instanceof TextComponentString) {
+								i1 += this.mc.fontRenderer
+										.getStringWidth(((TextComponentString) itextcomponent1).getText());
 
-                                if (i1 > i)
-                                {
-                                    return itextcomponent1;
-                                }
-                            }
-                        }
-                    }
+								if (i1 > i) {
+									return itextcomponent1;
+								}
+							}
+						}
+					}
 
-                    return null;
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            else
-            {
-                return null;
-            }
-        }
-    }
+					return null;
+				} else {
+					return null;
+				}
+			} else {
+				return null;
+			}
+		}
+	}
 
 	@SideOnly(Side.CLIENT)
 	static class NextPageButton extends GuiButton {
@@ -246,7 +223,7 @@ public class TCGuiTweet extends GuiScreen {
 		public NextPageButton(int buttonId, int x, int y, boolean isForwardIn) {
 			super(buttonId, x, y, 23, 13, "");
 			this.isForward = isForwardIn;
-			
+
 		}
 
 		/**
@@ -254,7 +231,8 @@ public class TCGuiTweet extends GuiScreen {
 		 */
 		public void drawButton(Minecraft mc, int mouseX, int mouseY, float partialTicks) {
 			if (this.visible) {
-				boolean flag = mouseX >= this.x && mouseY >= this.y && mouseX < this.x + this.width && mouseY < this.y + this.height;
+				boolean flag = mouseX >= this.x && mouseY >= this.y && mouseX < this.x + this.width
+						&& mouseY < this.y + this.height;
 				GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
 				mc.getTextureManager().bindTexture(TCGuiTweet.TEXTURE);
 				int i = 0;
