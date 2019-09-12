e9eb340d31096907e9c4d29456523b4c32258a92
Vasilis Pantelis <bpantelis10@gmail.com>
2019-09-12 13:41:29 +0300

    configuration change

--- TwitterCityMod/src/main/java/org/twittercity/twittercitymod/config/TwitterCityConfiguration.java
+++ TwitterCityMod/src/main/java/org/twittercity/twittercitymod/config/TwitterCityConfiguration.java
@@ -1,6 +1,6 @@
 package org.twittercity.twittercitymod.config;
 
 public class TwitterCityConfiguration {
-	public static boolean spawnImmediately = false;
+	public static boolean spawnImmediately = true;
 	public static boolean unbreakableCity = false;
 }
