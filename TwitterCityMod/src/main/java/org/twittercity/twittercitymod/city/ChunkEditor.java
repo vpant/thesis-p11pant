7a5ae181eacfa9e1ce7e259ff19b8344f3208796
Vasilis Pantelis <bpantelis10@gmail.com>
2019-10-12 22:49:28 +0300

    add world flattening using coordinates

--- TwitterCityMod/src/main/java/org/twittercity/twittercitymod/city/ChunkEditor.java
+++ TwitterCityMod/src/main/java/org/twittercity/twittercitymod/city/ChunkEditor.java
@@ -1,7 +1,5 @@
 package org.twittercity.twittercitymod.city;
 
-import org.twittercity.twittercitymod.TwitterCity;
-
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
@@ -10,13 +8,17 @@ import net.minecraft.world.World;
 // This class will manipulate the chunks our cities will built on (flatten the world etc)
 public class ChunkEditor {
 	private static void makeChunkFlat(World world, Block baseBlock, int chunkSourceX,int chunkSourceY, int chunkSourceZ) {
-		for(int x = chunkSourceX; x <= chunkSourceX + 16; x++) {
-			for(int z = chunkSourceZ; z <= chunkSourceZ + 16; z++) {
-				for(int y = chunkSourceY; y <= chunkSourceY; y++) {
+		makeAreaFlat(world, baseBlock, chunkSourceX, chunkSourceY, chunkSourceZ, 16, 16);
+	}
+	
+	private static void makeAreaFlat(World world, Block baseBlock, int sourceX, int sourceY, int sourceZ, int endX, int endZ) {
+		for(int x = sourceX; x <= sourceX + endX; x++) {
+			for(int z = sourceZ; z <= sourceZ + endZ; z++) {
+				for(int y = sourceY; y <= sourceY; y++) {
 					world.setBlockState(new BlockPos(x, y, z), baseBlock.getDefaultState());
 				}
 
-				for(int y = chunkSourceY + 1; y <= 128; y++) {
+				for(int y = sourceY + 1; y <= 128; y++) {
 					if(world.getBlockState(new BlockPos(x, y, z)).getBlock() != Blocks.AIR) {
 						world.destroyBlock(new BlockPos(x, y, z), false);
 					}
@@ -25,14 +27,18 @@ public class ChunkEditor {
 		}
 	}
 	
+	private static void makeAreaFlat(World world, Block baseBlock, BlockPos startingPos, int endXandZ) {
+		makeAreaFlat(world, baseBlock, startingPos.getX(), startingPos.getY(), startingPos.getZ(), endXandZ, endXandZ);
+	}
+	
 	/**
 	 * Make as many chunks necessary flat to fit the city
 	 * @param world
 	 * @param city
 	 */
-	public static void makeFlatChunksForCity(World world, City city) {
-		TwitterCity.logger.info("X is: {}, Y is: {} and Z is: {}", city.getStartingPos().getX(),city.getStartingPos().getY(), city.getStartingPos().getZ());
-		makeChunksFlat(world, city.getGroundBlock(), city.getStartingPos().getX(), city.getStartingPos().getY(), city.getStartingPos().getZ(), city.getChunkLength());
+	public static void makeFlatAreaForCity(World world, City city) {
+		//makeChunksFlat(world, city.getGroundBlock(), city.getStartingPos().getX(), city.getStartingPos().getY(), city.getStartingPos().getZ(), city.getCityLength()/16);
+		makeAreaFlat(world, city.getGroundBlock(), city.getStartingPos(), city.getCityLength());
 	}
 	
 	
@@ -50,5 +56,5 @@ public class ChunkEditor {
 				makeChunkFlat(world, baseBlock, chunkSourceX + (x * 16), chunkSourceY, chunkSourceZ + (z * 16));
 			}
 		}
-	}	
+	}
 }
